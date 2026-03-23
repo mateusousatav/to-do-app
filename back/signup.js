0bd3ead7e8eb2da2a871b0b1da55ed5d702d9c84
mateusousatav <mateusstavares2008@gmail.com>
2026-03-23 08:41:09 -0400

    x

--- back/signup.js
+++ back/signup.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
   signupForm.addEventListener("submit", async (event) => {
     event.preventDefault();
 
-    const username = document.getElementById("username").value;
+    // const username = document.getElementById("username").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
@@ -16,9 +16,8 @@ document.addEventListener("DOMContentLoaded", () => {
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
       const user = userCredential.user;
 
-      // Save user info to Firestore
       await setDoc(doc(db, "users", user.uid), {
-        username: username,
+        // username: username,
         email: email,
         createdAt: serverTimestamp()
       });
